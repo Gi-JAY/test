e48f7febb04f1fcdada80319f2ea13d319b09a46
Jay <asdeclf2@gmail.com>
2020-10-18 12:03:32 +0800

    修正nav

--- 版型開發/nodeTest/express.js
+++ 版型開發/nodeTest/express.js
@@ -6,15 +6,15 @@ const bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function (req, res) {
-   res.send(`First Name:${req.query.first_name}</br>Last Name:${req.query.last_name}`);
+  res.send(`First Name:${req.query.first_name}</br>Last Name:${req.query.last_name}`);
 })
 
-app.post('/',urlencodedParser,function(req,res){
-    let response = {
-        FirstName:req.body.first_name,
-        LastName:req.body.last_name
-    };
-    res.send(JSON.stringify(response));
+app.post('/', urlencodedParser, function (req, res) {
+  let response = {
+    FirstName: req.body.first_name,
+    LastName: req.body.last_name
+  };
+  res.send(JSON.stringify(response));
 })
 
 var server = app.listen(8081, function () {
